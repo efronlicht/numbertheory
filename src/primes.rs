65a5dd7ee14d383c8bf08429c382ed7470d25bf9
efron_licht <efron.python@gmail.com>
2018-03-02 19:28:24 -0800

    more idiomatic constructor for factors; new functions

--- src/primes.rs
+++ src/primes.rs
@@ -1,9 +1,111 @@
+use std::collections::HashMap;
 use std::collections;
 
+#[derive(Debug, Clone, PartialEq)]
 ///factors represents the prime factors of an integer. the key is the base, and the value is the exponent.
-///we note that since (1<<64-1) < (2<<64) is the max u64, the maximum possible exponent we can store without going into 
+///we note that since (1<<64-1) < (2<<64) is the max u64, the maximum possible exponent we can store without going into
 ///variable-precision arithmetic is 62, so a u8 is fine for the exponent.
-pub type Factors = collections::HashMap<u64, u8>;
+pub struct Factors(HashMap<u64, u8>);
+
+
+
+impl Factors {
+    ///find the prime factors of a positive integer, if any. 0 is considered to have no prime factorization. 1 is considered to have an empty prime factorization.
+    pub fn of(n: u64, primes: &Primes) -> Option<Self> {
+        let mut factors = HashMap::new();
+        match n {
+            0 => return None,
+            1 => return Some(Factors(factors)),
+            _ => {}
+        }
+        let mut n = n;
+
+        for p in &primes.0 {
+            let p = *p;
+            while n % p == 0 {
+                *factors.entry(p).or_insert(0) += 1;
+                n /= p;
+            }
+            if p > n {
+                return Some(Factors(factors)); //we know the prime factorization for sure
+            }
+        }
+        None // n is larger than our largest known prime. it could be prime, but it could also be a sufficiently large composite number
+    }
+
+    pub fn is_empty(&self) -> bool {
+        return self.0.is_empty();
+    }
+    pub fn mul(&self, rhs: &Self) -> Self {
+        let mut product = self.0.clone();
+        for (k, v) in &rhs.0 {
+            *product.entry(*k).or_insert(0) += v;
+        }
+        Factors(product)
+    }
+    
+    pub fn totient(&self) -> u64 {
+        if self.is_empty() {
+            return 0;
+        }
+        let mut totient = 1;
+        for (p, exp) in &self.0 {
+            totient *= p.pow(*exp as u32) - p.pow(*exp as u32 -1)
+        }
+        totient
+    }
+
+    pub fn is_subset(&self, other: &Self) -> bool {
+        for (base, exp) in &self.0 {
+            if other.get(*base) <= *exp {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn is_superset(&self, other: &Self) -> bool {
+        return other.is_subset(self);
+    }
+
+    pub fn get(&self, k: u64) -> u8 {
+        if let Some(exp) = self.0.get(&k) {
+            *exp
+        } else {
+            0
+        }
+    }
+
+    pub fn divisor_count(&self) -> u64 {
+        let mut divisors = 1;
+        for (_, exp) in &self.0 {
+            divisors *= *exp as u64 + 1;
+        }
+        divisors
+    }
+}
+
+impl IntoIterator for Factors {
+    type Item = (u64, u8);
+    type IntoIter = collections::hash_map::IntoIter<u64, u8>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl Into<u64> for Factors {
+    fn into(self) -> u64 {
+        self.into_iter()
+            .fold(1, |n, (base, exp)| n * (base.pow(exp as u32)))
+    }
+}
+
+impl Into<HashMap<u64, u8>> for Factors {
+    fn into(self) -> HashMap<u64, u8> {
+        return self.0
+    }
+}
+
 
 #[derive(Debug, Clone, PartialEq)]
 ///Primes represents a subset of the primes, starting with no gaps from the beginning of the positive integers, under a certain maximum.
@@ -11,12 +113,12 @@ pub type Factors = collections::HashMap<u64, u8>;
 pub struct Primes(Vec<u64>);
 
 impl Primes {
-    ///the maximum prime in the reperesentation
+    ///the maximum prime in the representation
     pub fn max(&self) -> Option<u64> {
         if self.0.len() == 0 {
             None
         } else {
-            Some(self.0[self.0.len()-1])
+            Some(self.0[self.0.len() - 1])
         }
     }
 
@@ -49,7 +151,7 @@ impl Primes {
     pub fn first_n(size: usize) -> Self {
         let mut primes = Primes(Vec::new());
         if size == 0 {
-            return primes
+            return primes;
         }
         primes.0.push(2);
         let mut m = 3;
@@ -66,18 +168,24 @@ impl Primes {
             }
 
             m += 2
-
         }
         primes
-}
-    
+    }
+
     ///convert a Vec<64> into Primes directly, without checking that they are actually prime.
-    pub unsafe fn from_raw_vec(v: Vec<u64>) -> Self {Primes(v)}
+    pub unsafe fn from_raw_vec(v: Vec<u64>) -> Self {
+        Primes(v)
+    }
 }
 
-impl Into<Vec<u64>> for Primes {fn into(self) -> Vec<u64> {self.0}}
+impl Into<Vec<u64>> for Primes {
+    fn into(self) -> Vec<u64> {
+        self.0
+    }
+}
 
 pub fn union(a: &Factors, b: &Factors) -> Factors {
+    let (a, b) = (&a.0, &b.0);
     let mut union = a.clone();
     for (k, v) in b {
         if !union.contains_key(k) {
@@ -87,28 +195,29 @@ pub fn union(a: &Factors, b: &Factors) -> Factors {
             union.insert(*k, m.max(*v));
         }
     }
-    union
+    Factors(union)
 }
 
 pub fn intersection(a: &Factors, b: &Factors) -> Factors {
-    let mut intersection = Factors::new();
+    let (a, b) = (&a.0, &b.0);
+    let mut intersection = HashMap::new();
     for (k, v) in a {
-        if b.contains_key(k) {
-            let min = v.min(&b[k]);
+        if b.contains_key(&k) {
+            let min = v.min(&b[&k]);
             intersection.insert(*k, *min);
         }
     }
-    intersection
+    Factors(intersection)
 }
 ///gcd is the greatest common divisor.  that is, the largest d such that ad == m bd == n for some positive integers a, b
 pub fn gcd(m: u64, n: u64, primes: &Primes) -> Option<u64> {
     match (m, n) {
         (0, _) | (_, 0) => None,
         (1, _) | (_, 1) => Some(1),
-        
+
         (m, n) => {
             let mut gcd = 1;
-            for (p, exp) in intersection(&factors(m, primes)?, &factors(n, primes)?) {
+            for (p, exp) in intersection(&Factors::of(m, primes)?, &Factors::of(n, primes)?) {
                 gcd *= p.pow(exp as u32);
             }
             Some(gcd)
@@ -119,13 +228,13 @@ pub fn gcd(m: u64, n: u64, primes: &Primes) -> Option<u64> {
 //lcm is the least common multiple; that is, the smallest q such that a*m == q, b*n == q for some positive integers a, b
 pub fn lcm(m: u64, n: u64, primes: &Primes) -> Option<u64> {
     match (m, n) {
-        (0, 0) => Some(0), 
-        (0, _) | (_, 0) => None, 
+        (0, 0) => Some(0),
+        (0, _) | (_, 0) => None,
         (m, 1) => Some(m),
         (1, n) => Some(n),
-        
+
         (m, n) => {
-            let union = union(&factors(n, primes)?, &factors(m, primes)?);
+            let union = union(&Factors::of(m, primes)?, &Factors::of(n, primes)?);
             let mut lcm = 1;
             for (p, exp) in union {
                 lcm *= p.pow(exp as u32);
@@ -133,28 +242,4 @@ pub fn lcm(m: u64, n: u64, primes: &Primes) -> Option<u64> {
             Some(lcm)
         }
     }
-}
-
-
-
-///find the prime factors of a positive integer, if any. 0 is considered to have no prime factorization. 1 is considered to have an empty prime factorization.
-pub fn factors(n: u64, primes: &Primes) -> Option<Factors> {
-    let mut factors = Factors::new();
-    match n {
-        0 => return None,
-        1 => return Some(factors),
-        _ => {},
-    }
-    let mut n = n;
-    for p in &primes.0 {
-        let p = *p;
-        while n % p == 0 {
-            *factors.entry(p).or_insert(0) += 1;
-            n /= p;
-            }
-        if p > n {
-            return Some(factors); //we know the prime factorization for sure
-            }   
-    }
-        None // n is larger than our largest known prime. it could be prime, but it could also be a sufficiently large composite number
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,9 @@
 use super::primes::*;
+use std::collections::HashMap;
 #[test]
 fn test_primes_under() {
     let got = Primes::under(12);
-    let want = unsafe{Primes::from_raw_vec(vec![2, 3, 5, 7, 11])};
+    let want = unsafe { Primes::from_raw_vec(vec![2, 3, 5, 7, 11]) };
     assert_eq!(want, got);
 }
 
@@ -10,22 +11,45 @@ fn test_primes_under() {
 fn test_primes_first_n() {
     let got = Primes::first_n(5);
     let want = Primes::under(12);
-    assert_eq!(want,got);
-    
+    assert_eq!(want, got);
 }
 
 #[test]
 fn test_factors() {
-    let got = factors(8 * 3, &Primes::under(12));
-    let want = Some(hashmap!{2 => 3, 3 => 1});
-    assert_eq!(want, got);
+    let got: HashMap<u64, u8> = Factors::of(8 * 3, &Primes::under(12)).unwrap().into();
+    let want = hashmap!{2 => 3, 3 => 1};
+    assert_eq!(got, want);
+}
+
+#[test]
+fn test_mul() {
+    let p = &Primes::first_n(10);
+    let f_12 = &Factors::of(2 * 2 * 3, p).unwrap();
+    let f_30 = &Factors::of(2 * 3 * 5, p).unwrap();
+    let got = f_12.mul(f_30);
+    assert_eq!(got, Factors::of(2 * 2 * 2 * 3 * 3 * 5, p).unwrap());
+}
+
+#[test]
+fn test_totient() {
+    let p = &Primes::first_n(20);
+    let factors = Factors::of(60, p).unwrap();
+    assert_eq!(16, factors.totient())
+}
+
+#[test]
+fn test_count_divisors() {
+    //the 12 divisors of 60 are 1,2,3,4,5,6,10,12,15,20,30,60
+    let p = &Primes::first_n(10);
+    let factors = Factors::of(60, p).unwrap();
+    assert_eq!(12, factors.divisor_count());
 }
 
 #[test]
 fn test_gcd() {
     // tests intersection implicitly
     let p = Primes::under(12);
-    let want =  Some(2 * 3 as u64);
+    let want = Some(2 * 3 as u64);
     let got = gcd(2 * 3 * 5, 2 * 2 * 2 * 3, &p);
     assert_eq!(got, want);
 
@@ -33,9 +57,8 @@ fn test_gcd() {
 
     assert_eq!(gcd(1, 90000, &p), Some(1));
     assert_eq!(gcd(90000, 1, &p), Some(1));
- 
+
     assert_eq!(gcd(0, 8, &p), None);
-       
 }
 
 #[test]
@@ -48,6 +71,5 @@ fn test_lcm() {
     assert_eq!(lcm(1, 5, &p), Some(5));
     assert_eq!(lcm(5, 5, &p), Some(5));
 
-    assert_eq!(lcm(23, 5, &p), None); // 23 is larger than our largest prime 
+    assert_eq!(lcm(23, 5, &p), None); // 23 is larger than our largest prime
 }
-
